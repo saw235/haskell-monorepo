a6933c31e5cbe3d71159618d705c37a44b45fcda
saw235 <saw235@users.noreply.github.com>
2025-07-20 19:11:09 +0000

    automated ormolu commit

--- haskell/app/tic-tac-toe/Server.hs
+++ haskell/app/tic-tac-toe/Server.hs
@@ -1,42 +1,44 @@
-{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards #-}
 
 module Server where
 
 import Control.Monad (unless, when)
 import Control.Monad.State (State, evalState, get, modify, put, runState)
-import Data.Aeson (FromJSON, ToJSON, encode, decode)
+import Data.Aeson (FromJSON, ToJSON, decode, encode)
+import qualified Data.ByteString.Char8 as BS
+import qualified Data.ByteString.Lazy as LBS
 import Data.List (intercalate, transpose)
 import Data.Maybe (fromJust, isJust)
 import GHC.Generics (Generic)
-import Network.Wai (Application, Response, responseLBS, requestMethod)
-import Network.Wai.Internal (getRequestBodyChunk)
-import Network.Wai.Handler.Warp (run)
-import Network.HTTP.Types (status200, status400, status404, methodPost, methodGet)
-import qualified Data.ByteString.Lazy as LBS
-import qualified Data.ByteString.Char8 as BS
-
 -- Re-export game types and logic from Main
 import qualified Main as Game
+import Network.HTTP.Types (methodGet, methodPost, status200, status400, status404)
+import Network.Wai (Application, Response, requestMethod, responseLBS)
+import Network.Wai.Handler.Warp (run)
+import Network.Wai.Internal (getRequestBodyChunk)
 
 -- JSON types for API communication
 data GameRequest = GameRequest
-  { action :: String
-  , position :: Maybe Game.Position
-  } deriving (Show, Generic)
+  { action :: String,
+    position :: Maybe Game.Position
+  }
+  deriving (Show, Generic)
 
 instance FromJSON GameRequest
+
 instance ToJSON GameRequest
 
 data GameResponse = GameResponse
-  { board :: [[String]]
-  , currentPlayer :: String
-  , gameOver :: Bool
-  , winner :: Maybe String
-  , message :: String
-  , validMoves :: [Game.Position]
-  } deriving (Show, Generic)
+  { board :: [[String]],
+    currentPlayer :: String,
+    gameOver :: Bool,
+    winner :: Maybe String,
+    message :: String,
+    validMoves :: [Game.Position]
+  }
+  deriving (Show, Generic)
 
 instance ToJSON GameResponse
 
@@ -63,7 +65,7 @@ data ServerState = ServerState
   }
 
 initialServerState :: ServerState
-initialServerState = ServerState { gameState = Game.initialGameState }
+initialServerState = ServerState {gameState = Game.initialGameState}
 
 -- Process game action
 processGameAction :: GameRequest -> ServerState -> (GameResponse, ServerState)
@@ -75,45 +77,47 @@ processGameAction request serverState = case action request of
   "get_state" -> (getStateResponse serverState, serverState)
   _ -> (errorResponse "Unknown action", serverState)
   where
-    newServerState = ServerState { gameState = Game.initialGameState }
-    createNewGameResponse = GameResponse
-      { board = boardToJson (Game.board Game.initialGameState)
-      , currentPlayer = show (Game.currentPlayer Game.initialGameState)
-      , gameOver = Game.gameOver Game.initialGameState
-      , winner = fmap show (Game.winner Game.initialGameState)
-      , message = "New game started"
-      , validMoves = Game.validMoves (Game.board Game.initialGameState)
-      }
+    newServerState = ServerState {gameState = Game.initialGameState}
+    createNewGameResponse =
+      GameResponse
+        { board = boardToJson (Game.board Game.initialGameState),
+          currentPlayer = show (Game.currentPlayer Game.initialGameState),
+          gameOver = Game.gameOver Game.initialGameState,
+          winner = fmap show (Game.winner Game.initialGameState),
+          message = "New game started",
+          validMoves = Game.validMoves (Game.board Game.initialGameState)
+        }
 
 makeMoveResponse :: Game.Position -> ServerState -> (GameResponse, ServerState)
-makeMoveResponse pos serverState = 
+makeMoveResponse pos serverState =
   let (result, newGameState) = runState (Game.gameEngine (Game.MakeMove pos)) (gameState serverState)
-      newServerState = ServerState { gameState = newGameState }
-  in case result of
-    Left errorMsg -> (errorResponse errorMsg, serverState)
-    Right _ -> (getStateResponse newServerState, newServerState)
+      newServerState = ServerState {gameState = newGameState}
+   in case result of
+        Left errorMsg -> (errorResponse errorMsg, serverState)
+        Right _ -> (getStateResponse newServerState, newServerState)
 
 getStateResponse :: ServerState -> GameResponse
-getStateResponse serverState = 
+getStateResponse serverState =
   let state = gameState serverState
-  in GameResponse
-    { board = boardToJson (Game.board state)
-    , currentPlayer = show (Game.currentPlayer state)
-    , gameOver = Game.gameOver state
-    , winner = fmap show (Game.winner state)
-    , message = "Current game state"
-    , validMoves = Game.validMoves (Game.board state)
-    }
+   in GameResponse
+        { board = boardToJson (Game.board state),
+          currentPlayer = show (Game.currentPlayer state),
+          gameOver = Game.gameOver state,
+          winner = fmap show (Game.winner state),
+          message = "Current game state",
+          validMoves = Game.validMoves (Game.board state)
+        }
 
 errorResponse :: String -> GameResponse
-errorResponse msg = GameResponse
-  { board = boardToJson Game.emptyBoard
-  , currentPlayer = "X"
-  , gameOver = False
-  , winner = Nothing
-  , message = msg
-  , validMoves = []
-  }
+errorResponse msg =
+  GameResponse
+    { board = boardToJson Game.emptyBoard,
+      currentPlayer = "X",
+      gameOver = False,
+      winner = Nothing,
+      message = msg,
+      validMoves = []
+    }
 
 -- WAI Application
 app :: ServerState -> Application
@@ -128,11 +132,9 @@ app serverState request respond = do
         Nothing -> do
           let errorResp = errorResponse "Invalid JSON"
           respond $ responseLBS status400 [("Content-Type", "application/json")] (encode errorResp)
-    
     (method, "GET") | method == methodGet -> do
       let response = getStateResponse serverState
       respond $ responseLBS status200 [("Content-Type", "application/json")] (encode response)
-    
     _ -> do
       let errorResp = errorResponse "Method not allowed"
       respond $ responseLBS status404 [("Content-Type", "application/json")] (encode errorResp)
@@ -141,13 +143,15 @@ app serverState request respond = do
 corsMiddleware :: Application -> Application
 corsMiddleware app request respond = do
   app request $ \response -> do
-    let headers = [("Access-Control-Allow-Origin", "*")
-                  , ("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
-                  , ("Access-Control-Allow-Headers", "Content-Type")]
+    let headers =
+          [ ("Access-Control-Allow-Origin", "*"),
+            ("Access-Control-Allow-Methods", "GET, POST, OPTIONS"),
+            ("Access-Control-Allow-Headers", "Content-Type")
+          ]
     respond response
 
 -- Start server
 startServer :: Int -> IO ()
 startServer port = do
   putStrLn $ "Starting Tic-Tac-Toe server on port " ++ show port
-  run port (corsMiddleware (app initialServerState)) 
+  run port (corsMiddleware (app initialServerState))
--- haskell/app/tic-tac-toe/ServerMain.hs
+++ haskell/app/tic-tac-toe/ServerMain.hs
@@ -6,4 +6,4 @@ main :: IO ()
 main = do
   putStrLn "Starting Tic-Tac-Toe Server..."
   putStrLn "Server will be available at http://localhost:3000"
-  startServer 3000 
+  startServer 3000
